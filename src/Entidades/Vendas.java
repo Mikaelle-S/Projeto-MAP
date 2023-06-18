106c774420bfd79eacea92ca5c252e83d68ecc96
Raquel Silveira <87213464+raquelsilveiraa@users.noreply.github.com>
2023-06-18 17:27:26 -0300

    Add files via upload

---
+++ src/Entidades/Vendas.java
@@ -0,0 +1,53 @@
+package Entidades;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Vendas {
+    private String hora;
+    private String data;
+    private Pedidos pedido;
+    
+    public Vendas() {
+    }
+
+    
+    public Vendas(Pedidos pedido) {
+        this.pedido = pedido;
+        this.data = LocalDate.now().toString();
+        this.hora = LocalTime.now().toString();
+    }
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public Pedidos getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedidos pedido) {
+        this.pedido = pedido;
+    }
+
+
+    @Override
+    public String toString() {
+        return  "Data: " + data + " Hora: " + hora + "\nVendas -> " + pedido;
+    }
+   
+}
+
